fa97424b873dea8e82d53d596d192b9e77662b3b
Prex52 <prexx1511@gmail.com>
2026-01-23 18:59:31 +0100

    Validation

--- serveur.js
+++ serveur.js
@@ -5,10 +5,11 @@ app.use(express.json());
 const SECRET = process.env.SECRET_KEY
 const PORT = process.env.PORT || 3000
 
-let pendingAction = null;
+let pendingAction =  [];
 
 app.post("/discord", (req, res) => {
   console.log("Clé reçue dans /discord :", req.body.key);
+
   if (req.body.key !== SECRET) {
     console.log("Tentative d'accès non autorisée à /discord");
     return res.sendStatus(403);
@@ -17,20 +18,25 @@ app.post("/discord", (req, res) => {
     console.log("Aucune action fournie");
     return res.status(400).send("Aucune action fournie");
   }
-  pendingAction = req.body.action;
+
+  pendingAction.push(req.body.action);
   console.log("Action stockée :", pendingAction);
   res.sendStatus(200);
 });
 
 app.get("/roblox", (req, res) => {
   console.log("Clé reçue dans /roblox :", req.query.key);
+
   if (req.query.key !== SECRET) {
     console.log("Tentative d'accès non autorisée à /roblox");
     return res.sendStatus(403);
   }
   res.json({ action: pendingAction });
-  pendingAction = null;
+  pendingAction = [];
 });
 
+app.get("/", (res) => {
+  res.send("API en ligne")
+});
 
 app.listen(PORT, () => console.log(`API running on port ${PORT}`));
